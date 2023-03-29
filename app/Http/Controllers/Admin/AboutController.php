0828f6180e388010fcecc3c44b948044f4d08611
rutvik mohite <40451097+rutvikmohite786@users.noreply.github.com>
2023-03-29 16:52:20 +0530

    port

---
+++ app/Http/Controllers/Admin/AboutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index(){
+       
+    }
+    public function store(){
+
+    }
+    public function edit(){
+
+    }
+    public function update(){
+       
+    }
+    public function delete(){
+      
+    }
+}
