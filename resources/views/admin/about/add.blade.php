6fd97f764a01a9dfb70a91f62f659ad5b7a4296c
rutvik mohite <40451097+rutvikmohite786@users.noreply.github.com>
2023-03-29 18:16:04 +0530

    hjh

--- resources/views/admin/about/add.blade.php
+++ resources/views/admin/about/add.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <div class="container">
 <form>
+  @csrf
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
